cbcfee9c73dffc73342fc1b1e7a9821325cb478b
lydiarachel <lydiarachel@gmail.com>
2018-07-16 18:51:50 -0400

    adding model text

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,5 +87,38 @@ $(document).ready(function () {
             nav.slideToggle(200);
         })
 
+/*Modal*/
+//get modal element
+var modal=document.getElementById("simpleModal");
+//get open modal button 
+var modalBtn = document.getElementById("modalBtn");
+//get close button
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+//isten for open click 
+modalBtn.addEventListener('click', openModal);
+
+//listen for close click 
+closeBtn.addEventListener('click', closeModal);
+
+//listen for outside click 
+window.addEventListener('click', clickOutside);
+
+//function to open Modal
+function openModal(){
+    modal.style.display = "block";
+}
+
+//function to close modal
+function closeModal(){
+    modal.style.display = "none";
+}
+
+//function to close modal if outside click
+function clickOutside(e){
+    if(e.target == modal){
+    modal.style.display = "none";
+    }
+}
 
 });
